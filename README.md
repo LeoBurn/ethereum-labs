584abda2331b4a6d2999a90fd250feccf42d5682
Leandro Viana <leandro.viana@xpand-it.com>
2022-01-13 16:39:00 +0000

    Add Transaction Submit

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # ethereum-labs
 Some Tests on blockchain ethereum
 
+
+Setup:
+- Needs to create .env file like this:
+    WALLET_PRIVATE_KEY="<PRIVATE KEY>"
+
 #Topics
  - Merkle Tree Implementation and Verify
  - Network block listener and display his information
+ - Transaction Sender Without Gas specification
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { BigNumber, providers, Wallet } from "ethers";
+import { BigNumber, providers, Wallet, utils } from "ethers";
 import  { merkleTree } from './merkleTree';
 
 
@@ -10,15 +10,20 @@ const provider = new providers.InfuraProvider(CHAIN_ID)
 const GWEI = BigNumber.from(10).pow(9)
 const ETHER = BigNumber.from(10).pow(18)
 
+async function getWallet() {
+    
+  if(process.env.WALLET_PRIVATE_KEY === undefined)
+  {
+      console.error("Please Provide WALLET_PRIVATE_KEY env")
+      process.exit(1);
+  }
+  const wallet = new Wallet(process.env.WALLET_PRIVATE_KEY,provider)
+  return wallet
+}
 
 async function main() {
 
-    if(process.env.WALLET_PRIVATE_KEY === undefined)
-    {
-        console.error("Please Provide WALLET_PRIVATE_KEY env")
-        process.exit(1);
-    }
-    const wallet = new Wallet(process.env.WALLET_PRIVATE_KEY,provider)
+
 
     provider.on('block',async (blockNumber)=>{
       console.log(blockNumber)
@@ -36,7 +41,34 @@ async function newBlockListener() {
     })
 }
 
+
+//Make transaction Without specify gas.
+async function makeTransaction(address,amount) {
+  const wallet = new Wallet(process.env.WALLET_PRIVATE_KEY,provider)
+  
+  //Get Wallet source
+  const addressWallet = await wallet.getAddress()
+  console.log('Wallet Source: ',addressWallet);
+
+  //Build the transaction
+  let transaction = {
+    to: address,
+    value: utils.parseEther(amount)
+  }
+  
+  //Send transaction and write the txHash
+  wallet.sendTransaction(transaction)
+        .then((txObj) => {
+          console.log('txHash', txObj.hash)
+        }
+  )
+  
+
+}
+
+
 //main()
 //merkleTree()
-newBlockListener()
+//newBlockListener()
+makeTransaction('0x2972915285416D0a923280f9786Fd85e6b50C17A','0.01');
 
